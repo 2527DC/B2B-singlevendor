3cbe70c4e89c93fb90e6cffac58c106afef6783b
root <root@bharatbuy.shop>
2026-02-18 05:46:16 +0000

    made a lot of changes remove unwanted file from git to track

--- storage/framework/views/69d99b49ccbd87ec0f9d6ca9b73d31b7.php
+++
@@ -1,7 +0,0 @@
-<script src="<?php echo e(asset(asset_path('frontend/amazy/compile_js/app.js'))); ?>"></script>
-
- <?php echo $__env->yieldPushContent('scripts'); ?>
-
-</body>
-
-</html><?php /**PATH /var/www/DhatriProduction/resources/views/frontend/amazy/auth/partials/_scripts.blade.php ENDPATH**/ ?>
--- storage/framework/views/b45d492fc7b5b70b63b3faf4e536cac5.php
+++
@@ -1,8 +0,0 @@
-<div class="main-title">
-    <h3 class="mb-30"> <?php echo e(__('frontendCms.add_InQuery')); ?> </h3>
-</div>
-
-<?php echo $__env->make('frontendcms::contact_content.components.query_form',['form_id' => 'add_query_form','form_tab' => 'iq_create','btn_id' => 'create_btn', 'button_name' => __('common.save') ], \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
-
-
-<?php /**PATH /var/www/DhatriProduction/Modules/FrontendCMS/Resources/views/contact_content/components/create_query.blade.php ENDPATH**/ ?>
